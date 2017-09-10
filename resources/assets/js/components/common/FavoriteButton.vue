aeb8b1ebdcce335981f80e28845eecb2bd73f86a
Zoltán Várkonyi <iamzozo@gmail.com>
2017-09-10 10:15:06 +0200

    Favorite button axios to http fiix

--- resources/assets/js/components/common/FavoriteButton.vue
+++ resources/assets/js/components/common/FavoriteButton.vue
@@ -1,52 +1,53 @@
 <template>
-    <a @click="toggleFavorite()" :class="[{'highlight': isFavorite.length}, 'btn']">
-        <i class="icon icon-star"></i>
-    </a>
+  <a @click="toggleFavorite()" :class="[{'highlight': isFavorite.length}, 'btn']">
+    <i class="icon icon-star"></i>
+  </a>
 </template>
 
 <script>
-    import {mapGetters} from 'vuex'
+  import { mapGetters } from 'vuex'
+  import http from '../../http'
 
-    export default {
-      props: ['type', 'id'],
-      computed: {
-        ...mapGetters({
-          favorites: 'favorites'
-        }),
-        isFavorite () {
-          return this.favorites.filter(f => {
-            return f.favoritable.id === this.id && f.type === this.type
+  export default {
+    props: ['type', 'id'],
+    computed: {
+      ...mapGetters({
+        favorites: 'favorites'
+      }),
+      isFavorite () {
+        return this.favorites.filter(f => {
+          return f.favoritable.id === this.id && f.type === this.type
+        })
+      }
+    },
+    methods: {
+      toggleFavorite () {
+        if (this.isFavorite.length) {
+          http.delete(`favorites/${this.type}/${this.id}`).then(() => {
+            this.$store.commit('deleteFavorite', {id: this.isFavorite[0].id})
           })
-        }
-      },
-      methods: {
-        toggleFavorite () {
-          if (this.isFavorite.length) {
-            axios.delete(`favorites/${this.type}/${this.id}`).then(() => {
-              this.$store.commit('deleteFavorite', {id: this.isFavorite[0].id})
-            })
-          } else {
-            axios.put(`favorites/${this.type}/${this.id}`).then(res => {
-              this.$store.commit('createFavorite', res.data)
-            })
-          }
+        } else {
+          http.put(`favorites/${this.type}/${this.id}`).then(res => {
+            this.$store.commit('createFavorite', res.data)
+          })
         }
       }
     }
+  }
 </script>
 
 <style lang="scss">
-    @import "../../../sass/variables";
+  @import "../../../sass/variables";
 
-    .highlight i {
-        color: $warn;
-    }
+  .highlight i {
+    color: $warn;
+  }
 </style>
 
 <style lang="scss">
-    @import "../../../sass/variables";
+  @import "../../../sass/variables";
 
-    .highlight i {
-        color: $warn;
-    }
+  .highlight i {
+    color: $warn;
+  }
 </style>

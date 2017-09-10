9f48986a217606b13bb63d4e004470075b001845
Zoltán Várkonyi <iamzozo@gmail.com>
2017-09-10 10:54:43 +0200

    Set editor text to empty

--- resources/assets/js/components/common/Editor.vue
+++ resources/assets/js/components/common/Editor.vue
@@ -1,47 +1,57 @@
 <template>
-    <div class="form-group">
-        <div id="editor" class="form-control">
-            <p v-html="value"></p>
-        </div>
+  <div class="form-group">
+    <div id="editor" class="form-control">
+      <p v-html="value"></p>
     </div>
+  </div>
 </template>
 
 <script>
-    import Quill from 'quill'
+  import Quill from 'quill'
 
-    export default {
-      props: {
-        value: {},
-        content: {
-          type: String,
-          default: () => ''
-        },
-        placeholder: {
-          type: String,
-          default: () => ''
-        }
+  export default {
+    props: {
+      value: {},
+      content: {
+        type: String,
+        default: () => ''
       },
-      data () {
-        return {
-          editor: null
-        }
-      },
-      mounted () {
-        const editorEl = this.$el.querySelectorAll('#editor')[0]
-        this.editor = new Quill(editorEl, {
-          modules: {
-            toolbar: [
-              ['bold', 'italic', 'link', 'code-block']
-            ]
-          },
-          placeholder: this.placeholder,
-          theme: 'snow'
-        })
-
-        this.editor.on('text-change', () => {
-          let content = this.editor.root.innerHTML.replace('<p><br></p>', '')
-          this.$emit('input', content)
-        })
+      placeholder: {
+        type: String,
+        default: () => ''
       }
+    },
+    data () {
+      return {
+        editor: null
+      }
+    },
+    mounted () {
+      const editorEl = this.$el.querySelectorAll('#editor')[0]
+      this.editor = new Quill(editorEl, {
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'link', 'code-block']
+          ]
+        },
+        placeholder: this.placeholder,
+        theme: 'snow'
+      })
+
+      this.editor.on('text-change', () => {
+        let content = this.editor.root.innerHTML.replace('<p><br></p>', '')
+        this.$emit('input', content)
+      })
+    },
+    methods: {
+      setText(n, o) {
+        if (n === '' && n !== o) {
+          this.editor.setText('')
+        }
+      }
+    },
+    watch: {
+      'value': 'setText'
     }
+  }
 </script>
